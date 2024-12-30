e0a2a9db0cf4742218e6bc46861de4ab0ffe2a49
SrinidhiS2099 <srinidhis.1si23mc050@gmail.com>
2024-12-30 21:34:56 +0530

    Update corsOptions.js

--- back-end/config/corsOptions.js
+++ back-end/config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
+const allowedOrigins = ['http://localhost:5173','http://localhost:5174','https://food-delivery-admin-8tqt.onrender.com']
 
 const corsOptions ={
     origin: (origin,callback)=>{
